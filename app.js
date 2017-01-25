1ec769cb1a4e6ab80811eb8d3ae4ccbc0a0a4fd7
edko <edisonko@gmail.com>
2017-01-25 11:08:24 -0800

    edit access-control-allow-origin

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ router.use(function(req, res, next){
 	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 	// Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	next();
 });
 
